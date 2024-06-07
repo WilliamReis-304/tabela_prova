8b7cd929ea5ddca23df4b587e81d7596a1255995
unknown <williamsil304@gmail.com>
2024-06-06 22:28:29 -0300

    version 1.0.1

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let alunos = Number(prompt("Digite a quantidade de alunos:"))
 let maior = 0
 let menor = 9999999999999
 let soma =0
-let contador = 0
 
 for (let i=0 ; i<alunos ; i++){
     let nota = Number(prompt("Digite a nota do aluno:"))
@@ -13,8 +12,7 @@ for (let i=0 ; i<alunos ; i++){
         menor = nota
     }
     soma += nota
-    contador++
 }
 
 console.log(`A maior nota da turma é : ${maior}\nA menor nota da turma é : ${menor}\n
-A média da turma é : ${soma/contador}\n`)
+A média da turma é : ${soma/alunos}\n`)
